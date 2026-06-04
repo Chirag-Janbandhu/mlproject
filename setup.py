2959d90511c4b03e5966567739e3511a1ac55315
janbandhuchirag1 <janbandhuchirag1@gmail.com>
2026-06-05 00:56:10 +0530

    Setup

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+from typing import List
+
+Hyphen_dot="-e ."
+
+def get_requirements(file_path:str)->List[str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+        
+        if Hyphen_dot in requirements:
+            requirements.remove(Hyphen_dot)
+            
+        return requirements
+    
+    
+
+
+setup(
+name="Mlproject",
+version="0.0.1",
+author="Chirag Janbandhu",
+author_email="chiragjanbandhu81@gmail.com",
+packages=find_packages(),
+install_requires=get_requirements("requirements.txt"),
+
+
+)
